a550f8c49b32ea820e9955eb3c58a561b9ab0485
Ekoramadhan13 <enugraha411@gmail.com>
2024-04-22 13:07:41 +0700

    menambahkan bagian input

--- pemogramandasar2204/pemogramandasar2204.cpp
+++ pemogramandasar2204/pemogramandasar2204.cpp
@@ -4,8 +4,16 @@ using namespace std;
 class bangunDatar {
 
 	//akses modifier
-private:
-	float panjang, lebar;
-public:
-	float luas;
+	private:
+		float panjang, lebar;
+	public:
+		float luas;
+
+		void input() { // methode input persegi panjang
+
+			cout << "Masukan panjangnya = ";
+			cin >> panjang;
+			cout << "Masukan lebarnya = ";
+			cin >> lebar;
+		}
 };
